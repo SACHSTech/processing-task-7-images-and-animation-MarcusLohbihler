7c617e65a46c7f26821a95cc5b5f1e4bc9121984
Marcus Lohbihler <75390882+MarcusLohbihler@users.noreply.github.com>
2022-04-20 01:37:55 +0000

    comments

--- Sketch.java
+++ Sketch.java
@@ -2,6 +2,7 @@ import processing.core.PApplet;
 import processing.core.PImage;
 
 public class Sketch extends PApplet {
+  //create image variables, and create X, Y, and other variables for animation
 	PImage imgUnderwater;
   PImage imgFish;
   int fishX = 25;
@@ -25,7 +26,8 @@ public class Sketch extends PApplet {
    * Called once at the beginning of execution.  Add initial set up
    * values here i.e background, stroke, fill etc.
    */
-  
+
+  //load ocean image, resize it to the size of the screen, and  also resize fish image
   public void setup() {
     background(210, 255, 173);
   imgUnderwater = loadImage("underwater-wallpaper-7116-7379-hd-wallpapers.jpg");
@@ -39,6 +41,7 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
+    //add ocean image as the background, loads fish which moves back and forth in a linear motion with collision detectors
 	  image(imgUnderwater, 0, 0);
     image(imgFish, fishX, fishY);
     fishX += fishSpeedX;
@@ -46,10 +49,9 @@ public class Sketch extends PApplet {
       fishSpeedX *= -1;
     }
 
-    
+    //creates green circle aka gem which moves in a circular motion around the fish
     fill(136, 252, 104);
     ellipse(gemX, gemY, 50, 50);
-
     gemX = (int)((Math.sin(time) + 1) * (width - 50) / 2 + 25);
     gemY = (int)((Math.cos(time) + 1) * (height - 50) / 2 + 25);
     time += 0.02;
